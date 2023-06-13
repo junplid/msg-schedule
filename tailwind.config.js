05f16da23c561e747a5f0dc6e1d78b708cccebd6
ousadojunplid <ousadojunplid@gmail.com>
2023-06-13 17:44:55 -0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    './pages/**/*.{html,js}',
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

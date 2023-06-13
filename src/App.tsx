cb598d77f5ca5b3b87f8160cf996eea44a299108
ousadojunplid <ousadojunplid@gmail.com>
2023-06-13 17:57:56 -0300

    config react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import PageLogin from "./pages/login";
+import PagePanel from "./pages/panel";
+
 export default function App() {
-  return <div className="text-2xl">ola mundo</div>;
+  return (
+    <Routes>
+      <Route
+        path="*"
+        element={
+          <>
+            <h2>Página não encontrada</h2>
+          </>
+        }
+      />
+      <Route path="/" element={<PageLogin />} />
+      <Route>
+        <Route path="/painel" element={<PagePanel />} />
+      </Route>
+    </Routes>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );

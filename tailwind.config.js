7daafc640a06122229bba48d029bf810e2ade704
ousadojunplid <ousadojunplid@gmail.com>
2023-06-13 19:44:35 -0300

    udpate color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
   ],
   theme: {
     extend: {},
+    backgroundColor: {
+      "1": "#000706",
+      "2": "#00272d",
+      "3": "#134647",
+      "4": "#0c7e7e",
+      "5": "#EBF1F2",
+    }
   },
   plugins: [],
 }

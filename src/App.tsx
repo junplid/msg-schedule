a8119227dd230596d93d168e35e0efbd442ea284
ousadojunplid <ousadojunplid@gmail.com>
2023-06-13 19:54:52 -0300

    create 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import PageLogin from "./pages/login";
 import PagePanel from "./pages/panel";
+import Page404 from "./pages/404";
 
 export default function App() {
   return (
     <Routes>
-      <Route
-        path="*"
-        element={
-          <>
-            <h2>Página não encontrada</h2>
-          </>
-        }
-      />
+      <Route path="*" element={<Page404 />} />
       <Route path="/" element={<PageLogin />} />
       <Route>
         <Route path="/painel" element={<PagePanel />} />

1f71639c3cd6004cc93e1486e9eddb2631961d49
ousadojunplid <ousadojunplid@gmail.com>
2023-06-14 00:51:48 -0300

    add news routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import PageLogin from "./pages/login";
 import PagePanel from "./pages/panel";
 import Page404 from "./pages/404";
 import LayoutDashboard from "./layout/dashboard";
+import PageMySectionWhatsApp from "./pages/my-section";
+import PageMessageWhatsApp from "./pages/message";
+import PageShotsWhatsApp from "./pages/shots";
 
 export default function App() {
   return (
@@ -10,7 +13,16 @@ export default function App() {
       <Route path="*" element={<Page404 />} />
       <Route path="/" element={<PageLogin />} />
       <Route path="/" element={<LayoutDashboard />}>
-        <Route path="/painel" element={<PagePanel />} />
+        <Route path="/panel" element={<PagePanel />} />
+        <Route
+          path="/panel/whatsapp/my-section"
+          element={<PageMySectionWhatsApp />}
+        />
+        <Route
+          path="/panel/whatsapp/message"
+          element={<PageMessageWhatsApp />}
+        />
+        <Route path="/panel/whatsapp/shots" element={<PageShotsWhatsApp />} />
       </Route>
     </Routes>
   );
